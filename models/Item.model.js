40e94fc63680053d9096630c65294ac7e4b9ff3f
julianBrandon <p0lterg3istt@gmail.com>
2022-10-08 13:25:17 +0200

    Add item.model

---
+++ models/Item.model.js
@@ -0,0 +1,28 @@
+const { Schema, model } = require('mongoose');
+
+const itemSchema = new Schema(
+    {
+        title: {
+            type: String,
+            required: true,
+            unique: true
+        },
+            price: {
+            type: Number,
+            required: true
+        },
+            
+            category: {
+                type: String,
+                enum: ["Starter", "Main", "Dessert", "Drink"]
+
+        },
+        description: String,
+
+    },
+    {
+        timestamps: true
+    }
+);
+
+module.exports = model('Item', itemSchema);
